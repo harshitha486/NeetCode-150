1908a02ff830c13744de59103da6646b6bba7a3d
harshitha486 <harshithamedepally@gmail.com>
2026-06-16 22:38:24 +0530

    added longest common prefix

---
+++ Arrays/longest_Common_Prefix.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+string longestCommonPrefix(vector<string>& strs) {
+    if (strs.empty()) return "";
+
+    string ans = "";
+
+    for (int i = 0; i < strs[0].size(); i++) {
+        char ch = strs[0][i];
+
+        for (int j = 1; j < strs.size(); j++) {
+            if (i >= strs[j].size() || strs[j][i] != ch) {
+                return ans;
+            }
+        }
+
+        ans += ch;
+    }
+
+    return ans;
+}
+
+int main() {
+    vector<string> strs = {"flower", "flow", "flight"};
+
+    cout << "Longest Common Prefix: "
+         << longestCommonPrefix(strs) << endl;
+
+    return 0;
+}
